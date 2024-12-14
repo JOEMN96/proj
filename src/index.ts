03eed9a2a0b3020342c6f71b6e348a4dc0bbf46c
Joe Mon <aruljoe37@gmail.com>
2024-12-14 16:27:25 +0530

    Added first two views

---
+++ src/index.ts
@@ -0,0 +1,3 @@
+import "./style.css";
+// import typescriptLogo from './typescript.svg'
+// import { setupCounter } from './counter.ts'
---
+++ src/pages/loginPage/login.ts
@@ -0,0 +1,10 @@
+const loginForm = <HTMLFormElement>document.querySelector(".login form");
+
+loginForm?.addEventListener("submit", (e) => {
+  e.preventDefault();
+  var formData = new FormData(loginForm);
+
+  if (formData.get("password") === "admin" && formData.get("userName") === "admin") {
+    window.location.replace(window.location.origin + "/choose-user-page.html");
+  }
+});
