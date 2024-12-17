b5655ea37c69bc33595150f3a58eab116cb727c6
Joe Mon <aruljoe37@gmail.com>
2024-12-17 17:11:09 +0530

    Added other pages

--- vite.config.js
+++ vite.config.js
@@ -1,3 +1,16 @@
-export default {
-  root: "./",
-};
+import { defineConfig } from "vite";
+import { resolve } from "path";
+
+export default defineConfig({
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, "index.html"),
+        chooseuser: resolve(__dirname, "src/pages/choose-user-page.html"),
+        assigner: resolve(__dirname, "src/pages/Assigner.html"),
+        Assignee: resolve(__dirname, "src/pages/Assignee.html"),
+        User: resolve(__dirname, "src/pages/User.html"),
+      },
+    },
+  },
+});
